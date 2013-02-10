9add95ad48302934045ce6bce4a6ed01951f2d91
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-02-10 16:35:45 +0100

    Define pin functions.
    Define all pin functions based on circuit.

--- bromoTimer.X/user.h
+++ bromoTimer.X/user.h
@@ -2,7 +2,36 @@
 /* User Level #define Macros                                                  */
 /******************************************************************************/
 
-/* TODO Application specific user parameters used in user.c may go here */
+/* Encoder */
+#define ENC_A RA0 /* Segnali quadratura encoder */
+#define ENC_B RA1
+#define ENC_SW RA5 /* Pulsante encoder */
+#define ENC_L1 RC6 /* LED 1 */
+#define ENC_L2 RC7 /* LED 2 */
+
+/* Comandi frontalino */
+#define EXT_SW RA2 /* Pulsante di accensione esterno */
+#define EXT_LED RA4 /* Led esterno */
+
+/* Sensore stato coperchio */
+#define EXT_LID RA3
+
+/* Comando buzzer */
+#define BUZZER RC5
+
+/* Comandi display LCD */
+#define DISPLAY_EN RC0 /* Display enable */
+#define DISPLAY_RS RC1 /* Display register select */
+#define DISPLAY_RW RC2 /* Display Read/Write */
+#define DISPLAY_B0 RB4
+#define DISPLAY_B1 RB5
+#define DISPLAY_B2 RB6
+#define DISPLAY_B3 RB7
+
+/* Controllo relè lampade */
+#define LAMP_1 RC4
+#define LAMP_2 RC3
+
 
 /******************************************************************************/
 /* User Function Prototypes                                                   */
