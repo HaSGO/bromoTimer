aabe2275124790f335a40a75ad5c05a6e4786835
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-03-18 17:31:25 +0100

    Add configuration word

--- bromoTimer.X/configuration_bits.c
+++ bromoTimer.X/configuration_bits.c
@@ -16,17 +16,13 @@
 /*                                                                            */
 /******************************************************************************/
 
-/* TODO Fill in your config bits here.  Remove #if 0 to embed config words.   */
-
-#if 0
-
-/* General syntax for configuration word 1 - Check your device .h file
-for an up to date listing of available macros.*/
-__CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & BOREN_OFF);
-
-/* If the device has multiple configuration words, the second macro defines
-the second configuration word.  Again check your device .h file
-for an up to date listing of available macros. */
-__CONFIG(WRT_OFF & PLLEN_OFF & STVREN_OFF & BORV_19 & LVP_OFF);
-
-#endif
+// CONFIG
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA4/OSC2/CLKOUT pin, I/O function on RA5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select bit (MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
