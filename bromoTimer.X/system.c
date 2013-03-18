06589b4cca3a8ad826e6de2c1d85325172187de6
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-03-18 17:32:18 +0100

    Configure internal RC oscillator

--- bromoTimer.X/system.c
+++ bromoTimer.X/system.c
@@ -12,9 +12,5 @@
 oscillator configurations. */
 void ConfigureOscillator(void)
 {
-    /* TODO Add clock switching code if appropriate.  */
-
-    /* Typical actions in this function are to tweak the oscillator tuning
-    register, select new clock sources, and to wait until new clock sources
-    are stable before resuming execution of the main project. */
+    OSCCON=0b01000001; /* Oscillatore interno, 1MHz clock*/
 }
