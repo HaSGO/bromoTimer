08e80802d97f1f9a83e42f98e6e92c0f7f5a9bf5
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2013-05-26 22:58:28 +0200

    Add self-test routine
    The self-test routine lights every available LED and clicks both relays to test
    everything.

--- bromoTimer.X/main.c
+++ bromoTimer.X/main.c
@@ -5,9 +5,12 @@
 #include <xc.h>           /* Global Header File */
 #include <stdint.h>        /* For uint8_t definition */
 #include <stdbool.h>       /* For true/false definition */
+#include <conio.h>
+#include <stdio.h>
 
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"          /* User funct/params, such as InitApp */
+#include "lcd.h"
 
 /******************************************************************************/
 /* User Global Variable Declaration                                           */
@@ -19,6 +22,26 @@
 /* Main Program                                                               */
 /******************************************************************************/
 
+
+inline void SelfTest() {
+    ENC_LR = 1;
+    __delay_ms(200);
+    ENC_LR = 0;
+    ENC_LG = 1;
+    __delay_ms(200);
+    ENC_LG = 0;
+    EXT_LED = 1;
+    __delay_ms(200);
+    EXT_LED = 0;
+    LAMP_1 = 1;
+    __delay_ms(200);
+    LAMP_1 = 0;
+    LAMP_2 = 1;
+    __delay_ms(200);
+    LAMP_2 = 0;
+    PlayBuzzerMs(200);
+}
+
 uint8_t main(void)
 {
     /* Configure the oscillator for the device */
@@ -27,11 +50,15 @@ uint8_t main(void)
     /* Initialize I/O and Peripherals for application */
     InitApp();
 
+    printf("\x2HaSGO Bromograph");
+    if (ENC_SW == 0) {
+        SelfTest();
+    }
+
     while(1)
     {
     	BuzzerPlayMs(200);
     	__delay_ms(200);
-
     }
     return 0;
 }
